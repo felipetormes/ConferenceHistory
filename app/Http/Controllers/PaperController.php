0caeba8161894e4989b49dd22514ce7cde1d6297
felipetormes <fbtormes@inf.ufrgs.br>
2016-11-17 15:19:38 -0200

    MODIFY: changes in the papers controller

--- app/Http/Controllers/PaperController.php
+++ app/Http/Controllers/PaperController.php
@@ -2,9 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Paper;
 use Illuminate\Http\Request;
 
 class PaperController extends Controller
 {
-    //
+    public function index()
+    {
+        $papers = Paper::all();
+
+        return view('papers.index', compact('papers'));
+    }
+
+    public function show($id)
+    {
+        $paper = Paper::find($id);
+        
+        return view('papers.info', compact('paper'));
+    }
 }
