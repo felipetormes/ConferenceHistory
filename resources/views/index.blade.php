71c11c8a6f99c84194ae87ec90f3d8ceb98ab779
felipetormes <fbtormes@inf.ufrgs.br>
2016-11-25 17:04:47 -0200

    MODIFY: use the Laravel Blate template in the index view

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,98 +1,17 @@
-<!DOCTYPE HTML>
-<html>
-<head>
-    <title>Find Papers</title>
-    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
-    <meta name="description" content="" />
-    <meta name="keywords" content="" />
-    <!--[if lte IE 8]><script src="js/html5shiv.js"></script><![endif]-->
-    <script src="js/jquery.min.js"></script>
-    <script src="js/skel.min.js"></script>
-    <script src="js/skel-layers.min.js"></script>
-    <script src="js/init.js"></script>
-    <noscript>
-        <link rel="stylesheet" href="css/skel.css" />
-        <link rel="stylesheet" href="css/style.css" />
-        <link rel="stylesheet" href="css/style-xlarge.css" />
-    </noscript>
-</head>
-<body id="top">
+@extends('model')
 
-<!-- Header -->
-<header id="header" class="skel-layers-fixed">
-    <h1><a href="/">Find Papers</a></h1>
-    <nav id="nav">
-        <ul>
-            <li><a href="/">Home</a></li>
-            <li><a href="#">About</a></li>
-            <li><a href="#">Search</a></li>
-            <li><a href="#">Contact</a></li>
-        </ul>
-    </nav>
-</header>
-<!-- Banner -->
-<section id="banner">
-    <div class="inner">
-        <h2>This is Find Papers</h2>
-        <p>Where you can find papers of many conferences</p>
-        <ul class="actions">
-            <li><a href="#login" class="button big special">Sign In</a></li>
-            <li><a href="#about" class="button big special">About</a></li>
-        </ul>
-    </div>
-</section>
+@section('title', 'Find Papers')
 
-<!-- One -->
-<section id="one" class="wrapper style1">
-    <header class="major">
-        <h2>Body</h2>
-        <p>Body Description</p>
-    </header>
-</section>
-
-<!-- Footer -->
-<footer id="footer">
-    <div class="container">
-        <div class="row double">
-            <div class="6u">
-                <div class="row collapse-at-2">
-                    <div class="6u">
-                        <h3>Accumsan</h3>
-                        <ul class="alt">
-                            <li><a href="#">Nascetur nunc varius</a></li>
-                            <li><a href="#">Vis faucibus sed tempor</a></li>
-                            <li><a href="#">Massa amet lobortis vel</a></li>
-                            <li><a href="#">Nascetur nunc varius</a></li>
-                        </ul>
-                    </div>
-                    <div class="6u">
-                        <h3>Faucibus</h3>
-                        <ul class="alt">
-                            <li><a href="#">Nascetur nunc varius</a></li>
-                            <li><a href="#">Vis faucibus sed tempor</a></li>
-                            <li><a href="#">Massa amet lobortis vel</a></li>
-                            <li><a href="#">Nascetur nunc varius</a></li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-            <div class="6u">
-                <h2>Aliquam Interdum</h2>
-                <p>Blandit nunc tempor lobortis nunc non. Mi accumsan. Justo aliquet massa adipiscing cubilia eu accumsan id. Arcu accumsan faucibus vis ultricies adipiscing ornare ut. Mi accumsan justo aliquet.</p>
-                <ul class="icons">
-                    <li><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
-                    <li><a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
-                    <li><a href="#" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
-                    <li><a href="#" class="icon fa-linkedin"><span class="label">LinkedIn</span></a></li>
-                    <li><a href="#" class="icon fa-pinterest"><span class="label">Pinterest</span></a></li>
-                </ul>
-            </div>
+@section('banner')
+    <!-- Banner -->
+    <section id="banner">
+        <div class="inner">
+            <h2>This is Find Papers</h2>
+            <p>Where you can find papers of many conferences</p>
+            <ul class="actions">
+                <li><a href="#login" class="button big special">Sign In</a></li>
+                <li><a href="#about" class="button big special">About</a></li>
+            </ul>
         </div>
-        <ul class="copyright">
-            <li>&copy; Untitled. All rights reserved.</li>
-        </ul>
-    </div>
-</footer>
-
-</body>
-</html>
+    </section>
+@endsection
