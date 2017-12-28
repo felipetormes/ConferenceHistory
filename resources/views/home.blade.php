4091dd55e05b1ef58c0acdb255c56f76392b6b90
Felipe Tormes <felipe.tormes@gmail.com>
2017-12-28 00:22:18 -0200

    Update home view.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,168 +1,203 @@
-@extends('layouts.app')
+<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
+<link href="https://cdn.datatables.net/1.10.16/css/dataTables.bootstrap.min.css" rel="stylesheet">
+<script src="https://code.jquery.com/jquery-1.12.4.js" type="text/javascript"></script>
+<script src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js" type="text/javascript"></script>
+<script src="https://cdn.datatables.net/1.10.16/js/dataTables.bootstrap.min.js" type="text/javascript"></script>
+<script src="js/bootstrap.min.js" type="text/javascript"></script>
 
-@section('title', 'Home')
+<nav class="navbar navbar-default navbar-static-top">
+    <div class="container">
+        <div class="navbar-header">
 
-@section('content')
+            <!-- Collapsed Hamburger -->
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
+                <span class="sr-only">Toggle Navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
 
-    <!-- Bootstrap Core CSS -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- MetisMenu CSS -->
-    <link href="css/metisMenu.min.css" rel="stylesheet">
-
-    <link href="css/font-awesome.min.css" rel="stylesheet" type="text/css">
-    <link href="css/rSlider.css" rel="stylesheet" type="text/css">
-    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
-    <link href="https://cdn.datatables.net/1.10.16/css/dataTables.bootstrap.min.css" rel="stylesheet">
-    <script src="https://code.jquery.com/jquery-1.12.4.js" type="text/javascript"></script>
-    <script src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js" type="text/javascript"></script>
-    <script src="https://cdn.datatables.net/1.10.16/js/dataTables.bootstrap.min.js" type="text/javascript"></script>
-
-
-    <div class="row">
-<div class="col-md-2 sidebar" id="fixed">
-<div class="mini-submenu">
-  <span class="icon-bar"></span>
-  <span class="icon-bar"></span>
-  <span class="icon-bar"></span>
-</div>
-<div class="list-group">
-  <span class="list-group-item active">
-      Conferences
-      <span class="pull-right" id="slide-submenu">
-          <i class="fa fa-times"></i>
-      </span>
-  </span>
-  @foreach($conferences as $conference)
-  <a class="list-group-item">
-     <label><input type="checkbox" id="{{ $conference->acronym }}" name="haha"/> {{ $conference->acronym }} </label>
-  </a>
-  @endforeach
-  <span class="list-group-item active">
-    Timeline
-  </span>
-  <a class="list-group-item">
-    <br/><br/>
-    <input type="range" id="sampleSlider" />
-  </a>
-</div>
-</div>
-
-<div class="col-md-2 list">
-</div>
-
-      <div class="col-md-9 list">
-
-        @foreach($conferences as $conference)
-        <div id="table{{ $conference->acronym }}" style="display:none">
-            <table width="100%" class="table table-striped table-bordered table-hover" id="{{ $conference->acronym }}">
-                <thead>
-                <tr>
-                    <th>First Name</th>
-                    <th>Middle Name</th>
-                    <th>Last Name</th>
-                    <th>Affiliation</th>
-                    <th>Papers</th>
-                    <th>Select</th>
-                </tr>
-                </thead>
-                <tbody>
-
-                  @foreach($persons as $person)
-                  <?php
-                    $flag = 0;
-                  ?>
-
-                  @foreach($conference->edition as $edition)
-                    @foreach($edition->papers as $paper)
-                    @foreach($paper->persons as $person1)
-                    <?php
-                    if($person1->id == $person->id) {
-                      $flag = 1;
-                    }
-                    ?>
-
-                    @endforeach
-                    @endforeach
-                    @endforeach
-
-                @if($flag)
-                <tr>
-                      <td>{{ $person->first_name }}</td>
-                      <td>{{ $person->middle_name }}</td>
-                      <td>{{ $person->last_name }}</td>
-                      <td>
-                        @foreach($person->institutions->unique() as $institution)
-                        {{ $institution->institution_name }} /
-                        @endforeach
-                      </td>
-                      <td>
-
-                        <?php
-                          $aux = 0;
-                          foreach ($person->papers as $paper) {
-                            foreach ($conference->edition as $edition) {
-                              if ($paper->edition_id == $edition->id) {
-                                $aux++;
-                              }
-                            }
-                          }
-                          echo $aux;
-                        ?>
-
-                      </td>
-                      <td>
-                        <input type="checkbox" id="{{ $person->id }}"/>
-                      </td>
-                </tr>
-
-                @endif
-                  @endforeach
-
-                </tbody>
-                </table>
+            <!-- Branding Image -->
+            <a class="navbar-brand" href="{{ url('/') }}">
+                {{ config('app.name') }}
+            </a>
         </div>
 
-        @endforeach
+        <div class="collapse navbar-collapse" id="app-navbar-collapse">
+            <!-- Left Side Of Navbar -->
+            <ul class="nav navbar-nav">
+                &nbsp;
+            </ul>
 
+            <!-- Right Side Of Navbar -->
+            <ul class="nav navbar-nav navbar-right">
+                <!-- Authentication Links -->
+                @if (Auth::guest())
+                    <li><a href="{{ url('#about') }}">About</a></li>
+                    <li><a href="{{ url('#contact') }}">Contact</a></li>
+                    <li><a href="{{ url('/login') }}">Login</a></li>
+                    <li><a href="{{ url('/register') }}">Register</a></li>
+                @else
+                    <li><a href="{{ url('/home') }}">Dashboard</a></li>
+                    <li><a href="{{ url('#about') }}">About</a></li>
+                    <li><a href="{{ url('#contact') }}">Contact</a></li>
+                    <li><a href="{{ url('/admin') }}">Admin</a></li>
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                            {{ Auth::user()->name }} <span class="caret"></span>
+                        </a>
 
+                        <ul class="dropdown-menu" role="menu">
+                            <li>
+                                <a href="{{ url('/logout') }}"
+                                    onclick="event.preventDefault();
+                                             document.getElementById('logout-form').submit();">
+                                    Logout
+                                </a>
+
+                                <form id="logout-form" action="{{ url('/logout') }}" method="POST" style="display: none;">
+                                    {{ csrf_field() }}
+                                </form>
+                            </li>
+                        </ul>
+                    </li>
+                @endif
+            </ul>
+        </div>
+    </div>
+</nav>
+
+<div class="row">
+
+<div class="col-md-1 list">
+</div>
+
+<div class="col-md-1 list">
+  <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+  Edit
+  </button>
+
+  <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          @foreach($conferences as $conference)
+         <a class="list-group-item">
+            <label><input type="checkbox" id="{{ $conference->acronym }}" name="haha"/> {{ $conference->acronym }} </label>
+         </a>
+         @endforeach
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        </div>
       </div>
-      </div>
+    </div>
+  </div>
+</div>
 
-    <script src="js/jquery.js" type="text/javascript"></script>
-    <script src="js/bootstrap.min.js" type="text/javascript"></script>
-    <script src="js/metisMenu.min.js" type="text/javascript"></script>
-    <script src="js/sb-admin-2.min.js" type="text/javascript"></script>
-    <script src="js/rSlider.js" type="text/javascript"></script>
+</div>
 
 
-    @foreach($conferences as $conference)
-      <script>
-          $('#{{ $conference->acronym }}').click(function() {
-              $("#table{{ $conference->acronym }}").toggle(this.checked);
-          });
-      </script>
+<div class="row">
+
+<div class="col-md-1 list">
+</div>
+
+<div class="col-md-10 list">
+
+@foreach($conferences as $conference)
+<div id="table{{ $conference->acronym }}" style="display:none">
+  <hr>
+  <h2 class="page-header">{{$conference->conference_title}} - {{ $conference->acronym }}</h2>
+<table id="dataTable{{ $conference->acronym }}" class="table table-striped table-bordered" cellspacing="0" width="100%">
+  <thead>
+          <tr>
+              <th>First Name</th>
+              <th>Middle Name</th>
+              <th>Last Name</th>
+              <th>Affiliation</th>
+              <th>Papers</th>
+              <th>Select</th>
+          </tr>
+          </thead>
+          <tbody>
+
+            @foreach($persons as $person)
+            <?php
+              $flag = 0;
+            ?>
+
+            @foreach($conference->edition as $edition)
+              @foreach($edition->papers as $paper)
+              @foreach($paper->persons as $person1)
+              <?php
+              if($person1->id == $person->id) {
+                $flag = 1;
+              }
+              ?>
+
+              @endforeach
+              @endforeach
+              @endforeach
+
+          @if($flag)
+          <tr>
+                <td>{{ $person->first_name }}</td>
+                <td>{{ $person->middle_name }}</td>
+                <td>{{ $person->last_name }}</td>
+                <td>
+                  @foreach($person->institutions->unique() as $institution)
+                  {{ $institution->institution_name }} /
+                  @endforeach
+                </td>
+                <td>
+
+                  <?php
+                    $aux = 0;
+                    foreach ($person->papers as $paper) {
+                      foreach ($conference->edition as $edition) {
+                        if ($paper->edition_id == $edition->id) {
+                          $aux++;
+                        }
+                      }
+                    }
+                    echo $aux;
+                  ?>
+
+                </td>
+                <td>
+                  <input type="checkbox" id="{{ $person->id }}"/>
+                </td>
+          </tr>
+
+          @endif
+            @endforeach
+
+          </tbody>
+    </table>
+  </div>
     @endforeach
+  </div>
+</div>
 
-    <script>
-      $(document).ready(function() {
-        $('#exemple').DataTable();
-      });
-    </script>
+  @foreach($conferences as $conference)
+     <script>
+         $('#{{ $conference->acronym }}').click(function() {
+             $("#table{{ $conference->acronym }}").toggle(this.checked);
+         });
+     </script>
+   @endforeach
 
-    <script>
-      var mySlider = new rSlider({
-        target: '#sampleSlider',
-        values: [1988, 1989, 1990, 1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017],
-        range: true,
-        tooltip: true,
-        scale: true,
-        labels: false,
-        set: [1988, 2017]
-      });
-    </script>
-
-
-
-
-@endsection
+@foreach($conferences as $conference)
+	<script>
+		$(document).ready(function() {
+			$('#dataTable{{ $conference->acronym }}').DataTable();
+		} );
+	</script>
+@endforeach
